381f0577d98dbc1866857944ccd0ed9a7a1667c8
JavaScript-Artist <mohammed.ghally110@gmail.com>
2024-06-20 12:18:41 -0700

    Abstract Logic For Game Component

--- src/Layouts/MainLayout.tsx
+++ src/Layouts/MainLayout.tsx
@@ -3,10 +3,10 @@ import styles from "../styles/components/MainLayout.module.scss";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className={styles.layoutContainer}>
+    <>
       <title>Guess Wave</title>
       <div className={styles.content}>{children}</div>
-    </div>
+    </>
   );
 };
 
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styles from "../styles/components/Game.module.scss";
 import Welcome from "./Welcome";
 import PointsControl from "./PointsControl";
@@ -9,153 +9,30 @@ import MultiplierChart from "./MultiplierChart";
 import UserInfo from "./UserInfo";
 import Chat from "./Chat";
 import Ranking from "./Ranking";
-
-interface ResultItem {
-  username: string;
-  score: number;
-}
-
-const generatePositiveGradientData = (target: number): { value: number }[] => {
-  const data: { value: number }[] = [];
-  let value = 0;
-  const steps = 5;
-  const totalIncrement = target / ((steps * (steps + 1)) / 2);
-
-  for (let i = 0; i < 3; i++) {
-    data.push({ value });
-  }
-
-  for (let i = 1; i <= steps; i++) {
-    value += i * totalIncrement;
-    data.push({ value });
-  }
-
-  if (data.length > 0) {
-    data[data.length - 1] = { value: target };
-  }
-
-  return data;
-};
+import useGame from "../hooks/useGame";
 
 const Game: React.FC = () => {
-  const [triggerAnimation, setTriggerAnimation] = useState(false);
-  const [username, setUsername] = useState("");
-  const [points, setPoints] = useState(100);
-  const [userPoints, setUserPoints] = useState(1000);
-  const [multiplier, setMultiplier] = useState(1);
-  const [speed, setSpeed] = useState(1);
-  const [result, setResult] = useState(0);
-  const [revealCurrentRoundResult, setRevealCurrentRoundResult] =
-    useState(false);
-  const [buttonState, setButtonState] = useState({
-    text: "Start",
-    disabled: false,
-  });
-  const [data, setData] = useState(generatePositiveGradientData(9));
-  const [records, setRecords] = useState<ResultItem[]>([]);
-  const [allResults, setAllResults] = useState<ResultItem[]>([]);
-  const [currentRound, setCurrentRound] = useState([
-    { username: username, points: points, multiplier: multiplier },
-    { username: "cpu0", points: 0, multiplier: 1 },
-    { username: "cpu1", points: 0, multiplier: 1 },
-    { username: "cpu2", points: 0, multiplier: 1 },
-    { username: "cpu3", points: 0, multiplier: 1 },
-  ]);
-
-  const handleNameSubmit = (username: string) => {
-    setCurrentRound([
-      { username: username, points: points, multiplier: multiplier },
-      { username: "cpu0", points: 0, multiplier: 1 },
-      { username: "cpu1", points: 0, multiplier: 1 },
-      { username: "cpu2", points: 0, multiplier: 1 },
-      { username: "cpu3", points: 0, multiplier: 1 },
-    ]);
-    setUsername(username);
-  };
-
-  const reEnableStartButton = () => {
-    setButtonState({ text: "Start Again", disabled: false });
-  };
-
-  const disableGraph = () => {
-    setTriggerAnimation(false);
-  };
-
-  const revealResult = () => {
-    setRevealCurrentRoundResult(true);
-  };
-
-  const handleStartClick = () => {
-    setUserPoints(userPoints - points);
-    const result_new = Math.floor(Math.random() * 90) / 10 + 1;
-    setResult(result_new);
-    setData(generatePositiveGradientData(result_new));
-    setTriggerAnimation(true);
-    setButtonState({ text: "Started", disabled: true });
-    setTimeout(reEnableStartButton, 2000);
-    setTimeout(disableGraph, 20000 / speed);
-
-    const roundResults = [
-      { username: username, points: points, multiplier: multiplier },
-      {
-        username: "cpu0",
-        points: Math.floor(Math.random() * 24 + 1) * 25,
-        multiplier: Math.floor(Math.random() * 100) / 10,
-      },
-      {
-        username: "cpu1",
-        points: Math.floor(Math.random() * 24 + 1) * 25,
-        multiplier: Math.floor(Math.random() * 100) / 10,
-      },
-      {
-        username: "cpu2",
-        points: Math.floor(Math.random() * 24 + 1) * 25,
-        multiplier: Math.floor(Math.random() * 100) / 10,
-      },
-      {
-        username: "cpu3",
-        points: Math.floor(Math.random() * 24 + 1) * 25,
-        multiplier: Math.floor(Math.random() * 100) / 10,
-      },
-    ];
-    setRevealCurrentRoundResult(false);
-
-    setTimeout(() => {
-      revealResult();
-      setCurrentRound(roundResults);
-    }, 12000 / speed);
-
-    if (result_new > multiplier) {
-      // won
-      setUserPoints(userPoints + points * multiplier);
-    }
-
-    const newResults: ResultItem[] = roundResults.map((result) => ({
-      username: result.username,
-      score: Math.floor(result.points * result.multiplier),
-    }));
-
-    const validResults = newResults.filter((result) => result.score > 0);
-
-    const uniqueResults = [...allResults, ...validResults].reduce<ResultItem[]>(
-      (acc, current) => {
-        const x = acc.find((item) => item.username === current.username);
-        if (!x) {
-          return acc.concat([current]);
-        } else {
-          return acc;
-        }
-      },
-      []
-    );
-
-    const topResults = uniqueResults
-      .sort((a, b) => b.score - a.score)
-      .slice(0, 5);
-
-    setAllResults(uniqueResults);
-    setRecords(topResults);
-  };
+  const {
+    triggerAnimation,
+    username,
+    setUsername,
+    points,
+    setPoints,
+    userPoints,
+    setUserPoints,
+    multiplier,
+    setMultiplier,
+    speed,
+    setSpeed,
+    result,
+    revealCurrentRoundResult,
+    buttonState,
+    data,
+    records,
+    currentRound,
+    handleNameSubmit,
+    handleStartClick,
+  } = useGame();
 
   return (
     <div className={styles.game}>
